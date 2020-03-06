dbd7972609dbe04dbf0ac667bc485189f8b0ef65
Jana Jarecki <jarecki.jana@gmail.com>
2020-03-06 13:20:15 +0100

    bugfixes

---
+++ R/meansd.R
@@ -0,0 +1,35 @@
+#' Compuate mean and (finite-sample corrected) standard deviation
+#'
+#' @param x A numeric vector
+#' @param na.rm  Logical, whether to remove \code{NA} values in \code{x}.
+#' @return A named list with the mean and the standard deviation
+#' @examples
+#' x <- sample(1:100)
+#' meansd(x)
+#' @export
+meansd <- function(x, na.rm = TRUE) {
+  res <- list(m = mean(x), sd = sd(x))
+  class(res) <- "meansd"
+  return(res)
+}
+
+
+#' Computes the mean and standard deviation and formats them as string
+#'
+#' @param x A numeric vector
+#' @param label (default \code{TRUE}) Logical, if true M and SD are added as labels
+#' @param na.rm Logical, whether to remove \code{NA} values in \code{x).
+#' @param digits The number of digits to print
+#' @return A string holding the mean m und standard deviation sd as \code{"m (sd)")" or \code{"M = m, SD = sd"}
+print.meansd <- function(x, digits = 2, na.rm = TRUE, label = FALSE){
+  if (all(is.na(x))) {
+    return(NA_character_)
+  }
+  x <- sprintf(unlist(x), fmt = paste0("%.", digits, "f"))
+  label <- if (label == TRUE) {
+    c("M=", ", SD=", "")
+  } else {
+    c("", " (SD=", ")")
+  }
+  cat(paste0(label[1], x[1], label[2], x[2], label[3]), "\n")
+}
